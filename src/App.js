d7c9c0483fe54accbfa76bab823041b488d97170
Dmitriy Kolomiec <kolomiec.dmitriy@yandex.ru>
2023-02-10 12:08:29 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Navigation from "./Components/Navigation/Nav";
+import "./styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div className="container">
+          <Navigation />
+          <Header />
+        </div>
       </header>
+      <main>
+        <div className="container">
+          <Main />
+        </div>
+      </main>
     </div>
   );
 }
