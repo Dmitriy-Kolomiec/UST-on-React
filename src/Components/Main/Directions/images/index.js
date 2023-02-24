c159ea01879dce5e667bd8b0be42d80b19e05f57
Dmitriy Kolomiec <kolomiec.dmitriy@yandex.ru>
2023-02-24 17:58:04 +0800

    edit Derections component

---
+++ src/Components/Main/Directions/images/index.js
@@ -0,0 +1,9 @@
+import dev from "./dev.png";
+import test from "./test.png";
+import support from "./support.png";
+import analitic from "./analitic.png";
+import model from "./prototype.png";
+import design from "./design.png";
+import arrow from "./Arrow_1.png";
+
+export {dev, test, support, analitic, model,design, arrow};
--- src/Components/Main/Directions/index.js
+++ src/Components/Main/Directions/index.js
@@ -1,12 +1,49 @@
 import React from "react";
 import styles from "./Directions.module.css";
-import devImg from "./images/dev.png";
-import testImg from "./images/test.png";
-import supportImg from "./images/support.png";
-import analiticImg from "./images/analitic.png";
-import prototypeImg from "./images/prototype.png";
-import designImg from "./images/design.png";
-import arrow from "./images/Arrow_1.png";
+import * as images from "./images/index";
+
+const DirectionItem = ({ image, title, text }) => {
+  return (
+    <div className={styles.direction}>
+      <img src={image} alt={title} />
+      <h4>{title}</h4>
+      <p className={styles.text}>{text}</p>
+    </div>
+  );
+};
+
+const directionsData = [
+  {
+    image: images.dev,
+    title: "Программируем",
+    text: "Мы можем разработать для вас любое индивидуальное решение. Наша компания обладает широкими возможностями для успешного создания технически-сложных систем.",
+  },
+  {
+    image: images.test,
+    title: "Тестируем",
+    text: "Проводим ручное функциональное тестирование и пишем UI-автотесты. Готовим чек-листы и тест-кейсы.",
+  },
+  {
+    image: images.support,
+    title: "Поддерживаем",
+    text: "Работаем по SLA. Следим за стабильностью работы приложения, обновляем под новые устройства и версии iOS и Android.",
+  },
+  {
+    image: images.analitic,
+    title: "Анализируем",
+    text: "Пишем ТЗ для разработки, учитывая бизнес-процессы и технологии заказчика и потребности пользователе",
+  },
+  {
+    image: images.model,
+    title: "Прототипируем",
+    text: "Прорабатываем пользовательские сценарии, проектируем логику приложения, скечируем основные экраны.",
+  },
+  {
+    image: images.design,
+    title: "Дизайним",
+    text: "Адаптируем фирменный стиль заказчика к гайдлайнам платформ. Рисуем удобные и понятные интерфейсы",
+  },
+];
 
 export const Directions = () => {
   return (
@@ -22,60 +59,22 @@ export const Directions = () => {
           </p>
         </div>
         <section className={styles.directions}>
-          <div className={styles.direction}>
-            <img src={devImg} alt="Значок" />
-            <h4>Программируем</h4>
-            <p className={styles.text}>
-              Создаём расширяемую архитектуру, пишем чистый и стабильный код.
-              Интегрируемся с технологиями заказчика.
-            </p>
-          </div>
-          <div className={styles.direction}>
-            <img src={testImg} alt="Значок" />
-            <h4>Тестируем</h4>
-            <p className={styles.text}>
-              Проводим ручное функциональное тестирование и пишем UI-автотесты.
-              Готовим чек-листы и тест-кейсы.
-            </p>
-          </div>
-          <div className={styles.direction}>
-            <img src={supportImg} alt="Значок" />
-            <h4>Поддерживаем</h4>
-            <p className={styles.text}>
-              Работаем по SLA. Следим за стабильностью работы приложения,
-              обновляем под новые устройства и версии iOS и Android.
-            </p>
-          </div>
-          <div className={styles.direction}>
-            <img src={analiticImg} alt="Значок" />
-            <h4>Анализируем</h4>
-            <p className={styles.text}>
-              Пишем ТЗ для разработки, учитывая бизнес-процессы и технологии
-              заказчика и потребности пользователей.
-            </p>
-          </div>
-          <div className={styles.direction}>
-            <img src={prototypeImg} alt="Значок" />
-            <h4>Прототипируем</h4>
-            <p className={styles.text}>
-              Прорабатываем пользовательские сценарии, проектируем логику
-              приложения, скечируем основные экраны.
-            </p>
-          </div>
-          <div className={styles.direction}>
-            <img src={designImg} alt="Значок" />
-            <h4>Дизайним</h4>
-            <p className={styles.text}>
-              Адаптируем фирменный стиль заказчика к гайдлайнам платформ. Рисуем
-              удобные и понятные интерфейсы.
-            </p>
-          </div>
+          {directionsData.map(({ image, title, text }, index) => {
+            return (
+              <DirectionItem
+                key={index}
+                image={image}
+                title={title}
+                text={text}
+              />
+            );
+          })}
         </section>
         <a href="#" className={styles.link__all__directions}>
           Все услуги
-          <img src={arrow} alt="arrow" />
+          <img src={images.arrow} alt="arrow" />
         </a>
       </section>
     </>
   );
-}
+};
